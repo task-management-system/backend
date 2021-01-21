3b3f336af44dc3c9942dc6bd436249e2be4c0e80
Atabayev Muslim <5106174-SkyfaceD@users.noreply.gitlab.com>
2021-01-22 01:07:23 +0600

    Добавить валидацию разрешений с бэка и локально

--- src/kz/tms/Application.kt
+++ src/kz/tms/Application.kt
@@ -4,12 +4,18 @@ import io.ktor.application.*
 import io.ktor.features.*
 import io.ktor.gson.*
 import io.ktor.server.netty.*
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kz.tms.database.data.permission.PermissionService
 import kz.tms.di.modules.applicationModule
 import kz.tms.di.modules.databaseModule
+import kz.tms.features.PermissionFeature
 import kz.tms.features.installAuthentication
 import kz.tms.features.installRouting
 import kz.tms.features.installStatusPages
+import kz.tms.utils.Permission
 import org.koin.ktor.ext.Koin
+import org.koin.ktor.ext.inject
 
 fun main(args: Array<String>) {
     EngineMain.main(args)
@@ -32,6 +38,9 @@ private fun Application.setKoin() {
 }
 
 private fun Application.setRestAPI() {
+    //TODO Переименовать
+    validatePermissions()
+
     //TODO Детальнее изучить обе фичи
     install(DefaultHeaders)
     install(CallLogging)
@@ -46,6 +55,42 @@ private fun Application.setRestAPI() {
     installStatusPages()
 
     installRouting()
+}
+
+private fun Application.validatePermissions() {
+    val permissionService: PermissionService by inject()
+    val permission: Permission.Companion by inject()
+
+    val scope = GlobalScope.launch {
+        val remotePermissions = permissionService.getAllAsPair()
+        val localPermissions = permission.pair
+
+        val remoteSize = remotePermissions.size
+        val localSize = localPermissions.size
+
+        if (remoteSize != localSize) throw RuntimeException(
+            """
+                Неверная длина разрешений 
+                Удаленно - $remoteSize
+                Локально - $localSize
+            """.trimIndent()
+        )
+
+        val remoteDiff = remotePermissions - localPermissions
+        val localDiff = localPermissions - remotePermissions
+
+        if (remoteDiff.isNotEmpty() || localDiff.isNotEmpty()) throw RuntimeException(
+            """
+                Различия в разрешениях
+                Удаленный список - $remotePermissions
+                Локальный список - $localPermissions
+                Разница удаленного списка - $remoteDiff
+                Разница локально списка   - $localDiff
+            """.trimIndent()
+        )
+    }
+
+    scope.start()
 }
 
 
--- src/kz/tms/di/modules/ApplicationModule.kt
+++ src/kz/tms/di/modules/ApplicationModule.kt
@@ -3,6 +3,7 @@ package kz.tms.di.modules
 import com.zaxxer.hikari.HikariDataSource
 import kz.tms.utils.ApplicationSettings
 import kz.tms.utils.JWTConfig
+import kz.tms.utils.Permission
 import org.koin.dsl.module
 import javax.sql.DataSource
 
@@ -12,4 +13,6 @@ val applicationModule = module(createdAtStart = true) {
     single<DataSource> { HikariDataSource(get<ApplicationSettings>().databaseConfig) }
 
     single { JWTConfig(jwtProperties = get<ApplicationSettings>().jwtProperties) }
+
+    single { Permission.Companion }
 }
