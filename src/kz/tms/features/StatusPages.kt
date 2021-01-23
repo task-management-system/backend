5d6718a74ca4921aec2d9e854e6f00ee9ff60e3d
Atabayev Muslim <5106174-SkyfaceD@users.noreply.gitlab.com>
2021-01-24 01:10:14 +0600

    Добавить обработку 404 ошибки

--- src/kz/tms/features/StatusPages.kt
+++ src/kz/tms/features/StatusPages.kt
@@ -8,9 +8,7 @@ import kz.tms.utils.error
 
 fun Application.installStatusPages() {
     install(StatusPages) {
-        /**
-         * Errors by status code
-         */
+        /** Errors by status code */
         status(HttpStatusCode.Unauthorized) { code ->
             call.error<Nothing>(
                 statusCode = code,
@@ -18,9 +16,14 @@ fun Application.installStatusPages() {
             )
         }
 
-        /**
-         * Errors by exception
-         */
+        status(HttpStatusCode.NotFound) { code ->
+            call.error<Nothing>(
+                statusCode = code,
+                message = "Страница не найдена"
+            )
+        }
+
+        /** Errors by exception */
         exception<PermissionException> { e ->
             call.error<Nothing>(
                 statusCode = HttpStatusCode.Forbidden,
