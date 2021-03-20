a0986b449acb3b3c06c183315cca31cc1fbb4c52
Atabayev Muslim <5106174-SkyfaceD@users.noreply.gitlab.com>
2021-03-20 13:32:12 +0600

    Рефакторинг отступов в файле Permission.kt

--- src/kz/tms/utils/Permission.kt
+++ src/kz/tms/utils/Permission.kt
@@ -11,12 +11,12 @@ class Permission(
      */
     companion object {
         //@formatter:off
-        val ViewTask   = Permission("ViewTask",   1 shl 0, "Просмотр задач"         )
-        val CreateTask = Permission("CreateTask", 1 shl 1, "Создание задач"         )
-        val DeleteTask = Permission("DeleteTask", 1 shl 2, "Удаление задач"         )
-        val ViewUser   = Permission("ViewUser",   1 shl 3, "Просмотр пользователя"  )
+        val ViewTask   = Permission("ViewTask",   1 shl 0, "Просмотр задач")
+        val CreateTask = Permission("CreateTask", 1 shl 1, "Создание задач")
+        val DeleteTask = Permission("DeleteTask", 1 shl 2, "Удаление задач")
+        val ViewUser   = Permission("ViewUser",   1 shl 3, "Просмотр пользователя")
         val InsertUser = Permission("InsertUser", 1 shl 4, "Добавление пользователя")
-        val DeleteUser = Permission("DeleteUser", 1 shl 5, "Удаление пользователя"  )
+        val DeleteUser = Permission("DeleteUser", 1 shl 5, "Удаление пользователя")
         val UpdateUser = Permission("UpdateUser", 1 shl 6, "Обновление пользователя")
         //@formatter:on
 
