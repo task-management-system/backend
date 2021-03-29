92836a799d4852293414e268dcd6aaee3fe57e88
Atabayev Muslim <5106174-SkyfaceD@users.noreply.gitlab.com>
2021-03-29 17:43:20 +0600

    Добавить утилиты для работы с exposed

--- src/kz/seasky/tms/extensions/ExposedExtensions.kt
+++ src/kz/seasky/tms/extensions/ExposedExtensions.kt
@@ -1,12 +1,46 @@
 package kz.seasky.tms.extensions
 
+import kz.seasky.tms.database.utils.Crypt
 import kz.seasky.tms.model.paging.Paging
-import org.jetbrains.exposed.sql.Column
-import org.jetbrains.exposed.sql.FieldSet
-import org.jetbrains.exposed.sql.Query
+import org.jetbrains.exposed.sql.*
+import org.jetbrains.exposed.sql.transactions.TransactionManager
+import java.sql.ResultSet
 
 fun FieldSet.selectAll(id: Column<Long>, paging: Paging): Query {
     return Query(this, null)
         .orderBy(id to paging.sortOrder)
         .limit(paging.limit, paging.offset)
+}
+
+/**
+ * Raw SQL example:
+ *  crypt('[password]', gen_salt('bf'))
+ */
+//TODO Rename
+fun crypt(password: String) = CustomStringFunction(
+    "crypt",
+    stringLiteral(password),
+    CustomStringFunction(
+        "gen_salt",
+        stringLiteral("bf")
+    )
+)
+
+
+/**
+ * Raw SQL example:
+ *  expr for example equals "user"."password"
+ *  expr = crypt('[password]', expr)
+ */
+//TODO Rename
+fun Column<*>.crypt(password: String): Crypt = Crypt(password, this)
+
+fun <T : Any> String.execAndMap(transform: (ResultSet) -> T): List<T> {
+    val result = arrayListOf<T>()
+    TransactionManager.current().exec(this) { rs ->
+        while (rs.next()) {
+            result += transform(rs)
+        }
+    }
+    return result
 }
