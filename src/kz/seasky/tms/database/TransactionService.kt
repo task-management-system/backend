fa67970b83da210eaf18f0cdf4b9e1a41f3d990c
Atabayev Muslim <5106174-SkyfaceD@users.noreply.gitlab.com>
2021-04-07 15:41:17 +0600

    Добавить возможность получать контекст текущей транзакции

--- src/kz/seasky/tms/database/TransactionService.kt
+++ src/kz/seasky/tms/database/TransactionService.kt
@@ -1,11 +1,13 @@
 package kz.seasky.tms.database
 
+import org.jetbrains.exposed.sql.Transaction
+
 interface TransactionService {
     suspend fun <T> newTransaction(block: suspend () -> T): T
 
     suspend fun <T> existingTransaction(block: suspend () -> T): T
 
-    suspend fun <T> transaction(block: suspend () -> T): T
+    suspend fun <T> transaction(block: suspend (transaction: Transaction) -> T): T
 }
 
 internal class TransactionServiceImpl(
@@ -20,7 +22,8 @@ internal class TransactionServiceImpl(
         block()
     }
 
-    override suspend fun <T> transaction(block: suspend () -> T) = databaseConnector.transaction {
-        block()
-    }
+    override suspend fun <T> transaction(block: suspend (transaction: Transaction) -> T) =
+        databaseConnector.transaction {
+            block(it)
+        }
 }
