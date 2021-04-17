912bce78c70071763e6f693e943f61af03d5a92b
Atabayev Muslim <5106174-SkyfaceD@users.noreply.gitlab.com>
2021-04-17 16:53:02 +0600

    Фикс силушки прав

--- src/kz/seasky/tms/utils/Permission.kt
+++ src/kz/seasky/tms/utils/Permission.kt
@@ -11,14 +11,14 @@ class Permission(
      */
     companion object {
         //@formatter:off
-        val Administrator = Permission("Administrator",1 shl 0, "Администрирование")
-        val ViewTask      = Permission("ViewTask",     1 shl 1, "Просмотр задач")
-        val CreateTask    = Permission("CreateTask",   1 shl 2, "Создание задач")
-        val DeleteTask    = Permission("DeleteTask",   1 shl 3, "Удаление задач")
-        val ViewUser      = Permission("ViewUser",     1 shl 4, "Просмотр пользователя")
-        val InsertUser    = Permission("InsertUser",   1 shl 5, "Добавление пользователя")
-        val DeleteUser    = Permission("DeleteUser",   1 shl 6, "Удаление пользователя")
-        val UpdateUser    = Permission("UpdateUser",   1 shl 7, "Обновление пользователя")
+        val Administrator = Permission("Administrator",1L shl 0, "Администрирование")
+        val ViewTask      = Permission("ViewTask",     1L shl 1, "Просмотр задач")
+        val CreateTask    = Permission("CreateTask",   1L shl 2, "Создание задач")
+        val DeleteTask    = Permission("DeleteTask",   1L shl 3, "Удаление задач")
+        val ViewUser      = Permission("ViewUser",     1L shl 4, "Просмотр пользователя")
+        val InsertUser    = Permission("InsertUser",   1L shl 5, "Добавление пользователя")
+        val DeleteUser    = Permission("DeleteUser",   1L shl 6, "Удаление пользователя")
+        val UpdateUser    = Permission("UpdateUser",   1L shl 7, "Обновление пользователя")
         //@formatter:on
 
         val all = allPermissions()
