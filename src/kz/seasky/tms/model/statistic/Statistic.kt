e5dc80eb89df007fd1dc04c94d9528675d794cdb
Atabayev Muslim <5106174-SkyfaceD@users.noreply.gitlab.com>
2021-04-26 17:08:06 +0600

    Добавить время создании сессии

--- src/kz/seasky/tms/model/statistic/Statistic.kt
+++ src/kz/seasky/tms/model/statistic/Statistic.kt
@@ -1,8 +1,11 @@
 package kz.seasky.tms.model.statistic
 
+import org.joda.time.DateTime
+
 data class Statistic(
     val task: Task,
-    val disk: Disk
+    val disk: Disk,
+    val createdAt: DateTime
 ) {
     data class Task(val all: Status, val actual: Status) {
         data class Status(
--- src/kz/seasky/tms/repository/statistic/StatisticService.kt
+++ src/kz/seasky/tms/repository/statistic/StatisticService.kt
@@ -2,6 +2,7 @@ package kz.seasky.tms.repository.statistic
 
 import io.ktor.sessions.*
 import kz.seasky.tms.model.statistic.Statistic
+import org.joda.time.DateTime
 
 interface StatisticService {
     suspend fun getAndUpdateSession(sessions: CurrentSession): Statistic
@@ -12,7 +13,8 @@ class StatisticServiceImpl(private val repository: StatisticRepository) : Statis
     override suspend fun getAndUpdateSession(sessions: CurrentSession): Statistic {
         val statistic = Statistic(
             task = Statistic.Task(repository.getTasksAll(), repository.getTasksActual()),
-            disk = Statistic.Disk(repository.getFileUsedSpace(), repository.getFileAvailableSpace())
+            disk = Statistic.Disk(repository.getFileUsedSpace(), repository.getFileAvailableSpace()),
+            createdAt = DateTime.now()
         )
 
         sessions.set(statistic)
